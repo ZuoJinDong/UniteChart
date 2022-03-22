ed42054976a5e8231b2cbfa1ac00261798d0efec
zuojindong <zjd77065163@qq.com>
2022-03-22 19:20:20 +0800

    main

--- app/src/main/java/com/zjd/demo/MainActivity.kt
+++ app/src/main/java/com/zjd/demo/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.zjd.demo
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.blankj.utilcode.util.ToastUtils
+import com.zjd.demo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("InflateParams")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.bind(layoutInflater.inflate(R.layout.activity_main, null))
+        setContentView(binding.root)
+    }
+
+    fun onClick(view: View) {
+        when(view.id){
+            R.id.btn_home -> {
+                ToastUtils.showShort("首页")
+            }
+        }
     }
 }
